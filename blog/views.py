1ea6560642baaed4fa96cf83ee97c3507811c96d
DanielAndresClavijo <contrutoxd@gmail.com>
2021-12-22 20:51:43 -0500

    Vista de blog y sus detalles

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import View
 
 from .forms import PostCreateForm
@@ -7,8 +7,9 @@ from .models import Post
 # Create your views here.
 class BlogListView(View):
   def get(self, request, *args, **kwargs):
+    posts = Post.objects.all()
     context = {
-
+      'posts':posts
     }
     return render(request, 'blog_list.html', context)
 
@@ -34,3 +35,11 @@ class BlogCreateView(View):
     context={
     }
     return render(request, 'blog_create.html', context)
+
+class BlogDetailView(View):
+  def get(self, request, pk, *args, **kwargs):
+    post = get_object_or_404(Post, pk=pk)
+    context={
+      'post':post
+    }
+    return render(request, 'blog_detail.html', context)
